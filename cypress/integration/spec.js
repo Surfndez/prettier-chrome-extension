ef6ef410084c1bf546003816beb03c50860676de
Nick McCurdy <nick@nickmccurdy.com>
2019-10-30 05:56:48 -0400

    Attempt to set up basic tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -9,10 +9,18 @@ describe("Prettier format button", () => {
       error => !error.message.includes("System is not defined")
     );
 
-    cy.visit("https://github.com/login");
+    cy.visit("https://github.com/login")
+      .pause()
+      .visit("https://github.com/prettier/prettier-chrome-extension/issues/new")
+      .pause()
+      .get(".prettier-btn")
+      .should("contain", "Prettier");
   });
 
   it("is injected on Stack Overflow", () => {
-    cy.visit("https://stackoverflow.com/questions/51875054");
+    cy.viewport("macbook-15");
+    cy.visit("https://stackoverflow.com/questions/51875054")
+      .get(".prettier-btn")
+      .should("contain", "Prettier");
   });
 });
